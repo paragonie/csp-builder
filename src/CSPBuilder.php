f580025496a36358c401341b5520915cf82d4967
Dan Ott <dan@dtott.com>
2023-01-19 14:47:38 -0500

    Add 'url' type value for report-uri

--- src/CSPBuilder.php
+++ src/CSPBuilder.php
@@ -137,7 +137,7 @@ class CSPBuilder
             if (!is_string($this->policies['report-uri'])) {
                 throw new TypeError('report-uri policy somehow not a string');
             }
-            $compiled [] = 'report-uri ' . $this->enc($this->policies['report-uri']) . '; ';
+            $compiled [] = 'report-uri ' . $this->enc($this->policies['report-uri'], 'url') . '; ';
         }
         if (!empty($this->policies['report-to'])) {
             if (!is_string($this->policies['report-to'])) {
