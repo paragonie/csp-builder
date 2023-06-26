343e087004bafd7afa7b93917c3a3a0345f7e9cb
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2023-06-26 20:00:20 +0200

    fix  'unsafe-hashed-attributes' in tests

--- test/ParserTest.php
+++ test/ParserTest.php
@@ -24,47 +24,47 @@ class ParserTest extends TestCase
         $this->assertSame($expected, $result);
     }
 
-    public function cspDirectivesProvider(): \Generator
+    public static function cspDirectivesProvider(): \Generator
     {
         yield [
             "default-src 'self'",
-            "default-src 'self'; "
+            "default-src 'self'"
         ];
         yield [
             "script-src 'none'",
-            "script-src 'none'; "
+            "script-src 'none'"
         ];
         yield [
             "script-src 'unsafe-eval'",
-            "script-src 'unsafe-eval'; "
+            "script-src 'unsafe-eval'"
         ];
         yield [
             "script-src 'unsafe-inline'",
-            "script-src 'unsafe-inline'; "
+            "script-src 'unsafe-inline'"
         ];
         yield [
             "style-src 'none'",
-            "style-src 'none'; "
+            "style-src 'none'"
         ];
         yield [
             "style-src 'self'",
-            "style-src 'self'; "
+            "style-src 'self'"
         ];
         yield [
             "style-src 'unsafe-inline'",
-            "style-src 'unsafe-inline'; "
+            "style-src 'unsafe-inline'"
         ];
         yield [
             "script-src 'self' example.com",
-            "script-src 'self' https://example.com http://example.com example.com; "
+            "script-src 'self' https://example.com http://example.com example.com"
         ];
         yield [
             "script-src 'self' example.com; style-src 'self'",
-            "script-src 'self' https://example.com http://example.com example.com; style-src 'self'; "
+            "script-src 'self' https://example.com http://example.com example.com; style-src 'self'"
         ];
         yield [
             "script-src 'self' example.com; style-src 'self' 'unsafe-inline'",
-            "script-src 'self' https://example.com http://example.com example.com; style-src 'self' 'unsafe-inline'; "
+            "script-src 'self' https://example.com http://example.com example.com; style-src 'self' 'unsafe-inline'"
         ];
         yield [
             "script-src 'self' example.com; style-src 'self' 'unsafe-inline'; upgrade-insecure-requests",
@@ -72,35 +72,35 @@ class ParserTest extends TestCase
         ];
         yield [
             "frame-ancestors 'none'; script-src 'self' example.com",
-            "frame-ancestors 'none'; script-src 'self' https://example.com http://example.com example.com; "
+            "frame-ancestors 'none'; script-src 'self' https://example.com http://example.com example.com"
         ];
         yield [
             "img-src 'self' data:; script-src 'self' example.com",
-            "img-src 'self' data:; script-src 'self' https://example.com http://example.com example.com; "
+            "img-src 'self' data:; script-src 'self' https://example.com http://example.com example.com"
         ];
         yield [
             "frame-ancestors 'self' https://example.org https://example.com https://store.example.com",
-            "frame-ancestors 'self' https://example.org https://example.com https://store.example.com; "
+            "frame-ancestors 'self' https://example.org https://example.com https://store.example.com"
         ];
         yield [
             "default-src 'self'; script-src https://example.com",
-            "default-src 'self'; script-src https://example.com; "
+            "default-src 'self'; script-src https://example.com"
         ];
         yield [
             "base-uri 'self'; report-uri https://endpoint.com; report-to csp-endpoint",
-            "base-uri 'self'; report-uri https://endpoint.com; report-to csp-endpoint; "
+            "base-uri 'self'; report-uri https://endpoint.com; report-to csp-endpoint"
         ];
         yield [
             "font-src https://example.com/",
-            "font-src https://example.com/; "
+            "font-src https://example.com/"
         ];
         yield [
             "script-src 'unsafe-hashed-attributes'",
-            "script-src 'unsafe-hashed-attributes'; "
+            "script-src 'unsafe-hashed-attributes'"
         ];
         yield [
             "plugin-types application/x-java-applet",
-            "plugin-types application/x-java-applet; "
+            "plugin-types application/x-java-applet"
         ];
     }
 }
